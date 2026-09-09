77d5a3d3f7259df805761738a21f246eee4a6a28
Sochima <humpheryprecious07@gmail.com>
2026-09-09 07:35:19 +0100

    feat: complete end-to-end signup route connection and configuration

--- Social Media Backend/server.js
+++ Social Media Backend/server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
+
 app.get("/api", (req, res) => {
   res.json({ message: "Welcome to the Social Media API!" });
 });
@@ -18,7 +22,7 @@ mongoose
   .then(() => console.log("Successfully connected to MongoDB!"))
   .catch((error) => console.error("MongoDB database connection error", error));
 
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server is currently running and listening on port ${PORT}`);
-  });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is currently running and listening on port ${PORT}`);
+});
