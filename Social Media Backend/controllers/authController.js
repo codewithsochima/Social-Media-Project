2aa9f35219bebcae74e6977357fa500b64f13516
Sochima <humpheryprecious07@gmail.com>
2026-09-09 07:02:22 +0100

    created auth aontrollers

---
+++ Social Media Backend/controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require("../models/user");
+
+exports.signup = async (req, res) => {
+  try {
+    const { first_name, last_name, username, email, password } = req.body;
+
+    const existingEmail = await User.findOne({ email });
+
+    if (existingEmail) {
+      return res.status(409).json({ error: "Email address is already in use" });
+    }
+
+    const newUser = new User({
+      first_name,
+      last_name,
+      username,
+      email,
+      password,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      user: {
+        id: newUser._id,
+        first_name: newUser.first_name,
+        last_name: newUser.last_name,
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
