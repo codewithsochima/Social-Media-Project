64bd2067b4a9fd30586ea465ceb44de5c20c31e9
Sochima <humpheryprecious07@gmail.com>
2026-09-09 08:31:09 +0100

    refactor: safe catch-block error handling inside user schema pre-save hook

--- Social Media Backend/models/user.js
+++ Social Media Backend/models/user.js
@@ -46,8 +46,7 @@ UserSchema.pre("save", async function () {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
   } catch (error) {
-    next(error)
+    throw error; 
   }
 });
-
 module.exports = mongoose.model("User", UserSchema);
